0f664ba6fbd81b590ee489d9f4cc10eb478d9c79
toastheo <weinreichth@gmail.com>
2023-10-28 21:47:36 +0200

    Updated DragAndDrop Script

--- ColdFeather Frenzy/Assets/Scripts/DragAndDrop.cs
+++ ColdFeather Frenzy/Assets/Scripts/DragAndDrop.cs
@@ -4,34 +4,25 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
-    private bool canMove;
-    private bool dragging;
+    private bool canMove = false;
+    private bool dragging = false;
 
     // for checking if mouse if over game object
     Collider2D mouseOverObject;
 
     // define frame
-    [SerializeField] private float maxX;
-    [SerializeField] private float minX;
-    [SerializeField] private float maxY;
-    [SerializeField] private float minY;
-
+    [SerializeField] private Vector2 maxBound = new(8, 4);
+    [SerializeField] private Vector2 minBound = new(-8, -4);
 
     // Start is called before the first frame update
     void Start()
     {
-
         mouseOverObject = GetComponent<Collider2D>();
-        canMove = false;
-        dragging = false;
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         if (Input.GetMouseButtonDown(0))
@@ -39,32 +30,23 @@ public class DragAndDrop : MonoBehaviour
             // 1 = righte Mouse BTN
             // 2 = middle Mouse BTN
         {
-            if (mouseOverObject == Physics2D.OverlapPoint(mousePos))
-            {
-                canMove = true;
-
-            }
-            else
-            {
-                canMove = false;
-
-            }
+            canMove = mouseOverObject == Physics2D.OverlapPoint(mousePos);
 
             if (canMove)
             {
                 dragging = true;
             }
-
         }
-
 
         if (dragging)
         {
-            // hold mouse postion inside values
-            float frameX = Mathf.Clamp(mousePos.x, minX, maxX);
-            float frameY = Mathf.Clamp(mousePos.y, minY, maxY);
+            // clamp and store mousePos
+            Vector2 clampPos = new(
+                Mathf.Clamp(mousePos.x, minBound.x, maxBound.x),
+                Mathf.Clamp(mousePos.y, minBound.y, maxBound.y)
+            );
 
-            this.transform.position = new Vector2(frameX, frameY);
+            transform.position = clampPos;
         }
 
         if (Input.GetMouseButtonUp(0))
