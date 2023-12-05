a7d4da45bde233108b7eb977971efa212e7b387d
Kurt <68389916+Bolzenpinguin@users.noreply.github.com>
2023-12-05 14:43:47 +0100

    Pulled Eggs one instance forward

--- Assets/Scripts/Lamps/DayNightConstant.cs
+++ Assets/Scripts/Lamps/DayNightConstant.cs
@@ -3,7 +3,7 @@ namespace Lamps
 {
     public static class DayNightConstant
     {
-        public const int DayStart = 8;
+        public const int DayStart = 7;
         public const int DayEnd = 16;
     
     
