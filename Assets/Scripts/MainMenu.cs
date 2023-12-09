af4ea10b2f3329afc7009ee18a810e256f1947ce
jorezst <jrs.stampa@gmail.com>
2023-12-09 16:28:58 +0100

    Added a GameOver Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
             SceneManager.LoadSceneAsync(1);
         }
 
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
