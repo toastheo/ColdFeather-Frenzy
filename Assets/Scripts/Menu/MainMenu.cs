889aa62e0f38260bfcdb731cdc74e56d3e70aebe
toastheo <weinreichth@gmail.com>
2023-12-17 16:22:43 +0100

    Added more sounds

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -7,11 +7,28 @@ public class MainMenu : MonoBehaviour
 {
      public void PlayGame()
      {
+        _ = StartCoroutine(PlayGameAfterAudio());
+     }
+
+    private IEnumerator PlayGameAfterAudio()
+    {
+        AudioSource startSound = GetComponent<AudioSource>();
+        startSound.Play();
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+
+        asyncLoad.allowSceneActivation = false;
+
+        yield return new WaitWhile(() => startSound.isPlaying);
+
+        yield return new WaitUntil(() => asyncLoad.progress >= 0.9f);
+
         GameLogicScript.score = 0;
         Time.timeScale = 1f;
         GameLogicScript.isGameOver = false;
-        SceneManager.LoadSceneAsync(1);
-     }
+
+        asyncLoad.allowSceneActivation = true;
+    }
 
     public void OpenMainMenu()
     {
